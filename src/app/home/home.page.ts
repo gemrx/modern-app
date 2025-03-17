2005bc41878cfbb8da4d2df6cd3a4e8f72c32506
gemr <gemrdev@gmail.com>
2025-03-17 16:22:29 -0500

    pruebas

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,64 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, inject } from '@angular/core';
+import { 
+  IonHeader, 
+  IonToolbar, 
+  IonTitle, 
+  IonContent, 
+  IonList,
+  InfiniteScrollCustomEvent, IonItem, IonAvatar, IonSkeletonText, IonAlert } from '@ionic/angular/standalone';
+import { MovieService } from '../service/movie.service';
+import { catchError, finalize } from 'rxjs';
+import { MovieResult } from '../service/interfaces';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonAlert, IonSkeletonText, IonAvatar, IonItem, IonHeader, IonToolbar, IonTitle, IonContent, IonList],
 })
 export class HomePage {
-  constructor() {}
+  private movieService = inject(MovieService);
+  private currentPage = 1;
+  public error = null;
+  public isLoading = false;
+  private movies: MovieResult[] = [];
+  private imageBaseUrl = 'https://image.tmdb.org./t/p';
+  public dummyAarray = new Array(5);
+
+  constructor() {
+    this.loadMovies()
+  }
+  
+
+  loadMovies(event?: InfiniteScrollCustomEvent) {
+    this.error = null;
+    if (!event) {
+      this.isLoading = true;
+    }
+    this.movieService.getTopRatedMovies(this.currentPage).pipe(
+      finalize(() => {
+        this.isLoading = false;
+        if (event) {
+          event.target.complete();
+        }
+      }),
+      catchError((err: any) => {
+        console.log(err);
+        
+        this.error = err.error.status_message;
+        return [];
+      })
+    ).subscribe({
+      next: (res) => {
+        console.log(res)
+
+        this.movies.push(...res.results);
+        if (event) {
+          event.target.disabled = res.total_pages === this.currentPage;
+        }
+      }
+    })
+  }
+
+  loadMore(event: InfiniteScrollCustomEvent) {}
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: '07a034c6606193bf0f812fc031452877'
 };
